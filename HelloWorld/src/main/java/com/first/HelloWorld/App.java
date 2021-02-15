e6fc84241da603c979ff8bafbd791126dbc43737
sreelakshmi665 <16691a0589@mits.ac.in>
2021-02-15 10:46:56 +0530

    commit

---
+++ HelloWorld/src/main/java/com/first/HelloWorld/App.java
@@ -0,0 +1,13 @@
+package com.first.HelloWorld;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
